36a40b253a0cb5bfed9405edea7e4835d48c1aa5
lakshikawijerathne111@gmail.com <laksh>
2019-07-26 20:46:23 +0530

    Saved

--- Q2.java
+++ Q2.java
@@ -3,13 +3,15 @@ public class Q2{
 	public static void main(String[] args){
 		int i,j,k;
 		
+		System.out.println("idiot");
 		Scanner p = new Scanner(System.in);
 		System.out.print("Enter number of rows for * phyramid:");
 		int x=p.nextInt();
 		
 		for(i=1;i<x+1;i++){
 			for(j=x-i;j>0;j--){
-				System.out.print(" ");
+				System.out.print("Hasini ");
+				System.out.println("idiot");
 			}
 			for(k=i;k>0;k--){
 				System.out.print("* ");
