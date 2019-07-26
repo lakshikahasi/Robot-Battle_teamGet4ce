12839d14415a6226c8f482c86a399d53d2f388e6
nishadisb <41675400+nishadisb@users.noreply.github.com>
2019-07-26 08:24:43 -0700

    Add files via upload

---
+++ Ary.java
@@ -0,0 +1,9 @@
+class Ary{
+public static void main(String[] args){
+	double[] myList={1.9,2.8,3.8,4.6};
+		for(double ele:myList){
+
+		System.out.println(ele);}
+
+}
+}
---
+++ Get4ce.java
@@ -0,0 +1,10 @@
+Class Get4ce{
+
+public static void main(String[] args){
+System.out.println("nishadi");
+System.out.println("nishadissfdsfs");
+
+}
+	
+	
+}
